014876ad8feb14c0e9dc0c21913823c8a4ba5c25
saraali <saraali.no3man@gmail.com>
2023-02-27 03:10:01 -0800

    ptrinting a string

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. i fear the lack of them - isaac Asimov";
+	_puts(str);
+	return (0);
+}
