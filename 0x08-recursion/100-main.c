2a773b80d9d40e62361e526750acc1f68d941fb0
saraali <saraali.no3man@gmail.com>
2023-03-08 00:40:54 -0800

    is palindrome

---
+++ 0x08-recursion/100-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	 = is_palindrome("level");
+	rintf("%d\n", r);
+	 = is_palindrome("redder");
+	rintf("%d\n", r);
+	 = is_palindrome("test");
+	rintf("%d\n", r);
+	 = is_palindrome("step on no pets");
+	pintf("%d\n", r);
+	return (0);
+}
