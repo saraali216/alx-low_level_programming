4d5a111995e992b5c331cd8f0e4cc7d7f62822fd
saraali <saraali.no3man@gmail.com>
2023-04-03 11:26:32 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = nw;
 		return (nw);
 	}
-	nw->next = tmp->nxt;
-	tmp->next = new;
+	nw->next = tmp->next;
+	tmp->next = nw;
 	return (nw);
 }
