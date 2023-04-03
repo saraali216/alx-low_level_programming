92dadbe8e6979e00286b897c372ca727bce47452
saraali <saraali.no3man@gmail.com>
2023-04-03 10:31:55 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -11,12 +11,12 @@
 
 void free_listint(listint_t *head)
 {
-	listint_t *n;
+	listint_t *next;
 
 	while (head != NULL)
 	{
-		n = head->n;
+		next = head->next;
 		free(head);
-		head = n;
+		head = next;
 	}
 }
