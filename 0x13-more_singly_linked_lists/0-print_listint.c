ac44f8af01779a59b903e71106ebff4757490793
saraali <saraali.no3man@gmail.com>
2023-04-03 10:09:17 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,13 +11,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t counter = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
+		counter++;
 	}
-	return (count);
+	return (counter);
 }
