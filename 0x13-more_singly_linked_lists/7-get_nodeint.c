5874b514ebf8449674b7e4ce903e3a554708a82c
saraali <saraali.no3man@gmail.com>
2023-04-03 11:15:42 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,11 +9,11 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
+	unsigned int j;
 
 	if (head == NULL)
 		return (NULL);
-	for (i = 0; i < index; i++)
+	for (j = 0; j < index; j++)
 	{
 		head = head->next;
 		if (head == NULL)
